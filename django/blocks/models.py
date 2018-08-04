5fa59e957897b4fa9b1e1eb9e6c82baf235ca725
Chan-Ho Suh <chanhosuh@gmail.com>
2018-08-03 23:31:18 -0400

    start models

--- django/blocks/models.py
+++ django/blocks/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 from model_utils.models import TimeStampedModel
 
-# Create your models here.
+
+if False:
+    import bitcoin.rpc
+    proxy = bitcoin.rpc.Proxy(service_url='http://user:password@bitcoind:8332')
+    block = proxy.getblock(proxy.getblockhash(0))
+    """
+    In [4]: block
+    Out[4]: CBlock(1, lx(0000000000000000000000000000000000000000000000000000000000000000), lx(4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b), 1231006505, 0x1d00ffff, 0x7c2bac1d)
+    """
 
 
 class Block(TimeStampedModel):
--- django/transactions/models.py
+++ django/transactions/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from model_utils.models import TimeStampedModel
 
-# Create your models here.
+
+class Transaction(TimeStampedModel):
+
+    txid = ...
+    size = ...
+    version = ...
+    locktime = ...
+
+    block = models.ForeignKey(
+        'blocks.Block',
+        related_name='transactions',
+        on_delete=models.CASCADE,
+        null=True,
+    )
+
+
+class TransactionInput(TimeStampedModel):
+
+    transaction = models.ForeignKey(
+        'transactions.Transaction',
+        related_name='vin',
+        on_delete=models.CASCADE,
+    )
+
+    txid = ...
+    vout = ...
+    script_sig = ...
+    sequence = ...
+
+
+class TransactionOutput(TimeStampedModel):
+
+    transaction = models.ForeignKey(
+        'transactions.Transaction',
+        related_name='vout',
+        on_delete=models.CASCADE,
+    )
+
+    value = models.IntegerField(null=False)
+    n = ...
+    script_pub_key = ...
