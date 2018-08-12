1900b832dec7b6a83727c6b87edd4f13c698db8b
Chan-Ho Suh <chanhosuh@gmail.com>
2018-08-11 22:10:17 -0400

    docstrings and some cleanup

--- django/jsonrpc/client.py
+++ django/jsonrpc/client.py
@@ -3,17 +3,41 @@ import requests
 
 from bitcoin_monitor import settings
 from . import RpcError
+import functools
 
 
 class RpcClientError(RpcError):
     pass
 
 
+def jsonify_response(func):
+
+    @functools.wraps(func)
+    def inner(*args, **kwargs):
+        response = func(*args, **kwargs)
+        if response.status_code not in [200, 500]:
+            raise RpcClientError(
+                str(response.status_code) + ' error: ' + response.reason
+            )
+        response_json = response.json()
+        if 'error' in response_json and response_json['error']:
+            raise RpcClientError(response_json['error'])
+
+        return response_json['result']
+
+    return inner
+
+
 # pylint: disable=redefined-outer-name
 class RpcClient:
     """ class to interact with bitcoind through
     its JSON-RPC API; essentially a substitute
-    for bitcoin-cli """
+    for bitcoin-cli.
+
+    The example code from this link was useful for
+    getting started:
+    https://kryptomusing.wordpress.com/2017/06/12/bitcoin-rpc-via-python/
+    """
 
     def __init__(self):
         self._rpc_url = settings.RPC_URL
@@ -28,6 +52,12 @@ class RpcClient:
         self._session.headers = settings.RPC_HEADERS
 
     def _call_method(self, rpc_method_name, rpc_params):
+        """
+        Posts a request using session and config data,
+        passing the RPC method name and parameters.
+
+        Returns a response object.
+        """
         params = {
             'jsonrpc': self._rpc_version,
             'id': self._rpc_id,
@@ -42,22 +72,25 @@ class RpcClient:
         )
         return response
 
+    @jsonify_response
     def get_best_block_hash(self):
-        response = self._call_method('getbestblockhash', rpc_params=[])
-        response_json = response.json()
-        return response_json['result']
+        return self._call_method('getbestblockhash', rpc_params=[])
 
+    @jsonify_response
     def get_block(self, block_hash, verbosity=1):
-        if verbosity not in [1, 2]:
-            raise RpcClientError(
-                "verbosity must be 1 or 2; for verbosity 0, use 'get_raw_block' instead"
-            )
-        response = self._call_method(
+        """
+        :param block_hash: string, hash of the wanted block
+        :param verbosity: int
+            0: raw byte string
+            1: JSON dict with 'tx' giving list of transaction hashes
+            2: same as 1 except 'tx' gives list of JSON transactions
+        :return: string,
+            hash or JSON, depending on 'verbosity' argument
+        """
+        return self._call_method(
             rpc_method_name='getblock',
             rpc_params=[block_hash, verbosity],
         )
-        response_json = response.json()
-        return response_json['result']
 
     def get_transactions(self, block_hash):
         block = self.get_block(block_hash, verbosity=2)
