531b32e7ced49637366a8bf1248daa7c036011a2
Chan-Ho Suh <chanhosuh@gmail.com>
2019-02-24 12:57:34 -0500

    improve unit tests

--- django/tests/bitcoin_monitor/test_tasks.py
+++ django/tests/bitcoin_monitor/test_tasks.py
@@ -1,10 +1,12 @@
 import json
 import os
 
+from django.forms.models import model_to_dict
 from django.test.testcases import TestCase
 
 from bitcoin_monitor.settings import BASE_DIR
 from bitcoin_monitor.tasks import process_block, process_transaction
+from blocks.models import Block
 
 
 class ProcessBlockchainTest(TestCase):
@@ -18,11 +20,19 @@ class ProcessBlockchainTest(TestCase):
         with open(filepath, 'r') as f:
             self.transactions = json.load(f)
 
+        self.block_hash = self.block['hash']
+
     def test_process_block(self):
         process_block.si(self.block).apply()
+        try:
+            block = Block.objects.get(hash=self.block_hash)
+        except Block.DoesNotExist:
+            self.fail(f'Block was not created with hash: {self.block_hash}')
+
+        block_dict = model_to_dict(block, exclude=['id', ])
+        self.assertDictEqual(self.block, block_dict)
 
     def test_process_transaction(self):
-        block_hash = self.block['hash']
         process_block.si(self.block).apply()
         for transaction in self.transactions:
-            process_transaction.si(transaction, block_hash).apply()
+            process_transaction.si(transaction, self.block_hash).apply()
