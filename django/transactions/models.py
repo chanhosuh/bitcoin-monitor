8c470a98e6a3ee6ddfe39b7fba5c836cb68ddafe
Chan-Ho Suh <chanhosuh@gmail.com>
2019-06-04 22:05:49 -0400

    finish parsing

--- django/transactions/models.py
+++ django/transactions/models.py
@@ -114,12 +114,26 @@ def parse_transaction(byte_stream):
 
 
 def parse_input(byte_stream):
-    prev_output = byte_stream.read(32)
-    prev_index_bytes = byte_stream.read(4)
+    prev_txid = byte_stream.read(32).hex()[::-1]
+    prev_index = int.from_bytes(byte_stream.read(4), 'little')
+
+    tx_input = TransactionInput(
+        txid=prev_txid,
+        vout=prev_index,
+    )
+    return tx_input
 
 
 def parse_output(byte_stream):
-    ...
+    value = int.from_bytes(byte_stream.read(8), 'little')
+    len_script_pubkey = int_from_varint(byte_stream)
+    script_pubkey = byte_stream.read(len_script_pubkey).hex()
+
+    tx_output = TransactionOutput(
+        value=value,
+        script_pubkey=script_pubkey,
+    )
+    return tx_output
 
 
 def hash256(byte_stream):
@@ -168,11 +182,6 @@ class TransactionInput(TimeStampedModel):
 
     sequence = models.BigIntegerField()
 
-    # "scriptSig": {     (json object) The script
-    #   "asm": "asm",  (string) asm
-    #   "hex": "hex"   (string) hex
-    # },
-    # "sequence": n      (numeric) The script sequence number
     script_sig = HexField(max_length=200)
 
 
@@ -187,14 +196,4 @@ class TransactionOutput(TimeStampedModel):
     value = BitcoinField()
     n = models.PositiveIntegerField()
 
-    # "scriptPubKey" : {          (json object)
-    #   "asm" : "asm",          (string) the asm
-    #   "hex" : "hex",          (string) the hex
-    #   "reqSigs" : n,            (numeric) The required sigs
-    #   "type" : "pubkeyhash",  (string) The type, eg 'pubkeyhash'
-    #   "addresses" : [           (json array of string)
-    #     "address"        (string) bitcoin address
-    #     ,...
-    #   ]
-    # }
-    # script_pub_key = models.CharField()
+    script_pubkey = HexField()
