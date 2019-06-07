7abb2253ed2d496f88379f92504eed4df86280a9
Chan-Ho Suh <chanhosuh@gmail.com>
2019-06-06 22:46:26 -0400

    docstring

--- django/bitcoin_monitor/tasks.py
+++ django/bitcoin_monitor/tasks.py
@@ -11,11 +11,10 @@ logger = logging.getLogger(__name__)
 @celery_app.task(bind=True, ignore_result=True, max_retries=1)
 def process_block(self, raw_block, height):  # pylint: disable=unused-argument
     """
-    :param block_data: dictionary
-        nested dictionary parsed from JSON,
-        most elements are key-value string pairs,
-        but some map a key to a list of
-        transaction dictionaries
+    :param raw_block: string
+        hex string for serialized block
+    :param height: integer
+        height on block chain (genesis block is 0)
     """
     raw_block = bytes.fromhex(raw_block)
     parse_block(raw_block, height)
