b366b9e30b268ca64f8c0b7170e936f98d86c74d
Chan-Ho Suh <chanhosuh@gmail.com>
2019-06-16 21:45:18 +0000

    django settings: add STATIC_ROOT

--- django/bitcoin_monitor/settings.py
+++ django/bitcoin_monitor/settings.py
@@ -134,9 +134,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Logging setup
 DEFAULT_LOGGING_FORMAT = '[%(levelname)s][%(name)s] %(asctime)s: %(message)s'
