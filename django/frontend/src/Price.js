32c01626d0622ed6564a7c0ac2bcac410d42c978
Chan-Ho Suh <chanhosuh@gmail.com>
2019-07-13 17:23:35 -0400

    add multiple websockets

--- django/frontend/src/Price.js
+++ django/frontend/src/Price.js
@@ -6,48 +6,64 @@ class Price extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			ticker: 'BTCUSD',
-			price: '',
-			change: '',
+			'BTCUSD': {
+				price: '',
+				change: '',
+			},
+			'ETHUSD': {
+				price: '',
+				change: '',
+			},
 		};
 	}
 
 	componentDidMount() {
 		const hostname = window.location.hostname;
-		this.connection = new WebSocket(
-			`ws://${hostname}/ws/BTC/USD`
-		);
-	
-		this.connection.onmessage = event => {
-			console.debug(event);
-			const data = JSON.parse(event.data);
-			if ('price' in data) {
-				this.setState({
-					price: data['price'],
-					change: data['price'] - this.state.price,
-				})
-			};
-			console.debug(this.state);
+		this.connections = {
+			'BTCUSD': new WebSocket(`ws://${hostname}:8000/ws/BTC/USD`),
+			'ETHUSD': new WebSocket(`ws://${hostname}:8000/ws/ETH/USD`),
 		};
+	
+		for (const ticker in this.connections) {
+			const conn = this.connections[ticker];
+			conn.onmessage = event => {
+				console.debug(event);
+				const data = JSON.parse(event.data);
+				if ('price' in data) {
+					let new_state = this.state;
+					new_state[ticker] = {
+						price: data['price'],
+						change: data['price'] - this.state[ticker].price,
+					};
+					this.setState(new_state);
+				};
+				console.debug(this.state);
+			};
+		}
 	}
  
 	render() {
-		const ticker = this.state.ticker;
-		const price = this.state.price;
-		const classes = this.state.change >= 0 ? 'panel Price-display-increase' : 'panel Price-display-decrease';
+		var rows = [];
+		for (const ticker in this.state) {
+			const classes = this.state[ticker].change >= 0 ? 'panel Price-display-increase' : 'panel Price-display-decrease';
+			const price = this.state[ticker].price;
 
+			rows.push(
+			  <div className="row" key={ticker}>
+				  <div className="col-md-7">
+					 <div className="panel panel-purple">{ticker}</div>   
+				  </div>
+				  <div className="col-md-5">
+					  <div id={ticker} className={classes} key={Math.random()}>{price}</div>
+				  </div>
+			  </div>
+			)
+		}
 		return (
-				<div class="col-sm-8">
-					<div class="panel panel-blue">Markets
-				          <div class="row">
-				              <div class="col-md-7">
-				                 <div class="panel panel-purple">{ticker}</div>   
-				              </div>
-				              <div class="col-md-5">
-				              	<div id={ticker} className={classes} key={Math.random()}>{price}</div>
-				              </div>
-				          </div>
-				     </div>
+				<div className="col-sm-8">
+					<div className="panel panel-blue">Markets
+						{rows}
+					</div>
 				 </div>
 		);
 	}
