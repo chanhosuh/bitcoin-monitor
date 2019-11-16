f3f63337ee27a10c2ffa19ed41298dff9ab44369
Chan-Ho Suh <chanhosuh@gmail.com>
2019-11-16 01:03:54 -0500

    move frontend outside django directory
    - this necessitates having npm postbuild move the build artifacts
      to django/react/build
    - updated gitignore to ignore the new static files
    - updated react path in django settings

--- django/bitcoin_monitor/settings.py
+++ django/bitcoin_monitor/settings.py
@@ -19,7 +19,7 @@ import requests
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-REACT_APP_DIR = os.path.join(BASE_DIR, 'frontend')
+REACT_APP_DIR = os.path.join(BASE_DIR, 'react')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = (
