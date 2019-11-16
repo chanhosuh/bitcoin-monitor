85f56e3d6989adbfd90a5d473254c98e6f6b5f49
Chan-Ho Suh <chanhosuh@gmail.com>
2019-11-15 22:35:05 -0500

    update css

--- django/frontend/src/ExampleWrapper.js
+++ django/frontend/src/ExampleWrapper.js
@@ -37,7 +37,19 @@ const ExampleWrapper = ({
     if (!isItemLoaded(index)) {
       return <div style={style}>Loading ... </div>;
     } else {
-      const { height, hash, age } = items[index];
+      /*
+      age: "10 years, 9 months ago"
+      bits: "ffff001d"
+      hash: "00000000f8246f56ac5bf25d799247bac3d0b1349ba72c105092f5a022a48f68"
+      height: 2118
+      merkle_root: "97919e93371b8648967e1e1be9d30511cf642903bd9b388f51acd4debc306fb0"
+      nonce: "1cbe648e"
+      num_transactions: 1
+      prev_hash: "0000000087fcd3a97d43df5fea343a41e1dfdbe0a4cc5f61bbebf9589fd7e01d"
+      timestamp: 1233150472
+      version: 1
+      */
+      const { height, hash, age, num_transactions, timestamp } = items[index];
       return (
         <div style={style}>
           <div className="row">
