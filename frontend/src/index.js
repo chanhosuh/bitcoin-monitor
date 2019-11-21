af05ea545a05e319f57bdcbba8180115d48e80a3
Chan-Ho Suh <chanhosuh@gmail.com>
2019-11-20 19:14:42 -0500

    add normalize.css

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "normalize.css";
 import "./index.css";
 import App from "./main/App.js";
 import * as serviceWorker from "./serviceWorker";
