28447020cfb5793abb865b8f3b47b34d875af942
Chan-Ho Suh <chanhosuh@gmail.com>
2019-11-30 20:02:43 -0500

    remove deprecated tests

--- django/tests/bitcoin_monitor/test_tasks.py
+++ django/tests/bitcoin_monitor/test_tasks.py
@@ -1,37 +1,5 @@
-import json
-import os
-
-from django.forms.models import model_to_dict
 from django.test.testcases import TestCase
-
-from bitcoin_monitor.settings import BASE_DIR
-from bitcoin_monitor.tasks import process_block, process_transaction
 
 
 class ProcessBlockchainTest(TestCase):
-
-    def setUp(self):
-        filepath = os.path.join(BASE_DIR, 'tests', 'data', 'block.json')
-        with open(filepath, 'r') as f:
-            self.block = json.load(f)
-
-        filepath = os.path.join(BASE_DIR, 'tests', 'data', 'transactions.json')
-        with open(filepath, 'r') as f:
-            self.transactions = json.load(f)
-
-        self.block_hash = self.block['hash']
-
-    def test_process_block(self):
-        block = process_block.si(self.block).apply().result
-        self.assertEqual(
-            block.hash,
-            '00000000000000000e83bd062de52ed1ca17699a697ffdd53bd867c5c39a6eb0',
-        )
-
-        block_dict = model_to_dict(block, exclude=['id', ])
-        self.assertDictEqual(self.block, block_dict)
-
-    def test_process_transaction(self):
-        block = process_block.si(self.block).apply().result
-        for transaction in self.transactions:
-            process_transaction.si(transaction, block).apply()
+    pass
