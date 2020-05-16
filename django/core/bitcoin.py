27274d7fbe0201b59e55624e8939da5a835b488a
Chan-Ho Suh <chanhosuh@gmail.com>
2020-05-15 22:36:25 -0400

    Make Bitcoin a wrapper around integer
    - store bitcoin amount as satoshis, but create a Decimal version
      to be used in division.  Also allow any constructor arg type
      that we can easily quantize to Decimal.
    - add Bitcoin.to_bytes
    - various other fixes

--- django/core/bitcoin.py
+++ django/core/bitcoin.py
@@ -4,10 +4,6 @@ from functools import total_ordering
 from django.utils.deconstruct import deconstructible
 
 
-def quantize_to(dec, places):
-    return dec.quantize(Decimal(10) ** (-1 * places), rounding=ROUND_HALF_EVEN)
-
-
 class BitcoinError(RuntimeError):
     pass
 
@@ -24,76 +20,94 @@ class BitcoinValueError(ValueError, BitcoinError):
 @total_ordering
 class Bitcoin:
     """
-    A wrapper class around Decimal, to ensure that allowed arithmetic operations
-    result in a Decimal quantized to the correct number of decimal places,
-    with appropriate rounding.
-
     Conceptually, a Bitcoin object disallows some arithmetic operations (such as
     multiplying Bitcoin by Bitcoin) while allowing others (adding Bitcoin to Bitcoin or
     taking a fraction of Bitcoin).
+
+    Since there is a minimum unit of bitcoin, a satoshi, it makes sense to do
+    arithmetic operations on satoshis (integers) as much as possible, while using
+    appropriately quantized Decimals when needed.  This class wraps around both
+    integer and Decimal amounts appropriately.
     """
 
     DECIMAL_PLACES = 8  # minimal unit is 1e-8, a "satoshi"
 
     def __init__(self, value):
+        """ value (int): amount in satoshis """
         try:
-            self.decimal = self.quantize_to_currency(Decimal(value))
-        except (InvalidOperation, TypeError):
+            value = self.quantize(value)
+            self.satoshis = int(value)
+            self.decimal = value / 10 ** self.DECIMAL_PLACES
+        except (ValueError, TypeError, InvalidOperation):
             raise BitcoinValueError(
                 "Canot create Bitcoin with value '{}' of type {}".format(
                     value, type(value).__name__,
                 )
             )
 
+    def to_bytes(self, *args, **kwargs):
+        """
+        Convenience method, since serialization is everywhere in Bitcoin!
+        Since the usual format sent over the wire is satoshis encoded
+        as bytes, we delegate to the 'satoshis' attribute.
+        """
+        return self.satoshis.to_bytes(*args, **kwargs)
+
     def _is_bitcoin(self, operand):
         return isinstance(operand, Bitcoin)
 
     def __eq__(self, other):
         if self._is_bitcoin(other):
-            return self.decimal == other.decimal
+            return self.satoshis == other.satoshis
         return False
 
     def __lt__(self, other):
         if self._is_bitcoin(other):
-            return self.decimal < other.decimal
+            return self.satoshis < other.satoshis
         return NotImplemented
 
     def __hash__(self):
-        return hash(self.decimal)
+        return hash(self.satoshis)
 
     def __pos__(self):
         return self
 
     def __neg__(self):
-        return Bitcoin(-1 * self.decimal)
+        return Bitcoin(-1 * self.satoshis)
 
     def __add__(self, other):
         if not self._is_bitcoin(other):
-            raise BitcoinTypeError('Cannot add Bitcoin object to non-Bitcoin object.')
-        return Bitcoin(self.decimal + other.decimal)
+            raise BitcoinTypeError("Cannot add Bitcoin object to non-Bitcoin object.")
+        return Bitcoin(self.satoshis + other.satoshis)
 
     def __sub__(self, other):
         if not self._is_bitcoin(other):
-            raise BitcoinTypeError('Cannot subtract non-Bitcoin object from Bitcoin object.')
-        return Bitcoin(self.decimal - other.decimal)
+            raise BitcoinTypeError(
+                "Cannot subtract non-Bitcoin object from Bitcoin object."
+            )
+        return Bitcoin(self.satoshis - other.satoshis)
 
     def __rsub__(self, other):
         if not self._is_bitcoin(other):
-            raise BitcoinTypeError('Cannot subtract Bitcoin object from non-Bitcoin object.')
-        return Bitcoin(other.decimal - self.decimal)
+            raise BitcoinTypeError(
+                "Cannot subtract Bitcoin object from non-Bitcoin object."
+            )
+        return Bitcoin(other.satoshis - self.satoshis)
 
     def __mul__(self, other):
         if self._is_bitcoin(other):
-            raise BitcoinTypeError('Cannot multiply Bitcoin object by another Bitcoin object.')
-        return Bitcoin(self.decimal * other)
+            raise BitcoinTypeError(
+                "Cannot multiply Bitcoin object by another Bitcoin object."
+            )
+        return Bitcoin(self.satoshis * other)
 
     def __truediv__(self, other):
         if isinstance(other, Bitcoin):
             return self.decimal / other.decimal
-        return Bitcoin(self.decimal / other)
+        return Bitcoin(self.satoshis / other)
 
     def __rtruediv__(self, other):  # pylint: disable=unused-argument
-        raise BitcoinTypeError('Cannot divide non-Bitcoin object by Bitcoin object.')
+        raise BitcoinTypeError("Cannot divide non-Bitcoin object by Bitcoin object.")
 
     __radd__ = __add__
     __rmul__ = __mul__
@@ -105,9 +119,7 @@ class Bitcoin:
         return str(self.decimal)
 
     @classmethod
-    def quantize_to_currency(cls, dec):
-        return quantize_to(dec, cls.DECIMAL_PLACES)
-
-    # Decimal methods
-    def as_tuple(self):
-        return self.decimal.as_tuple()
+    def quantize(cls, dec):
+        return dec.quantize(
+            Decimal("10") * (-1 * cls.DECIMAL_PLACES), rounding=ROUND_HALF_EVEN
+        )
--- django/core/model_fields.py
+++ django/core/model_fields.py
@@ -1,5 +1,6 @@
 from django.core.exceptions import ValidationError
 from django.db import models
+from django.db.models.fields import BigIntegerField
 from django.utils.translation import ugettext_lazy as _
 
 from .bitcoin import Bitcoin
@@ -18,35 +19,15 @@ def validate_hex(hex_string):
 
 
 class HexField(models.CharField):
+    description = _("Hex encoding as a string.")
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.validators.append(validate_hex)
 
 
-class ModifiedDecimalBaseField(models.DecimalField):
-    """Base class for modified versions of DecimalField."""
-
-    def __init__(self, *args, **kwargs):
-        kwargs["decimal_places"] = self.DECIMAL_PLACES  # pylint: disable=no-member
-        kwargs["max_digits"] = self.MAX_DIGITS  # pylint: disable=no-member
-        super().__init__(*args, **kwargs)
-
-    def deconstruct(self):
-        name, path, args, kwargs = super().deconstruct()
-        del kwargs["decimal_places"]
-        del kwargs["max_digits"]
-        return name, path, args, kwargs
-
-
-class BitcoinField(ModifiedDecimalBaseField):
-    """
-    Standardized Decimal representation of bitcoin amount.
-    """
-
-    description = _("Bitcoin as a standardized Decimal number.")
-
-    DECIMAL_PLACES = Bitcoin.DECIMAL_PLACES
-    MAX_DIGITS = 16
+class BitcoinField(BigIntegerField):
+    description = _("Bitcoin amount in satoshis.")
 
     # pylint: disable=unused-argument
     def from_db_value(self, value, expression, connection):
@@ -59,7 +40,7 @@ class BitcoinField(ModifiedDecimalBaseField):
             return Bitcoin(value)
         except (TypeError, ValueError):
             raise ValidationError(
-                "value {} must be a Decimal or a string representing one.".format(value)
+                "value {} must be an integer or string representing one.".format(value)
             )
 
     def get_prep_value(self, value):
@@ -67,27 +48,10 @@ class BitcoinField(ModifiedDecimalBaseField):
         Prepares value for parameter in query.
         The value type is fairly flexible, but floats are explicitly not allowed.
         """
-        if isinstance(value, Bitcoin):
-            return value.decimal
-        elif type(value) == float:
-            raise TypeError("Cannot use float with BitcoinField")
-        elif value is None:
+        if value is None:
             return None
-        return Bitcoin(value).decimal
-
-    def get_db_prep_save(self, value, connection):
-        """
-        Optionally overridden by some model fields, for value types that need
-        special handling before saving.  Normally, by default, it just calls
-        'get_prep_value' (see above).  DecimalField, in particular,
-        uses this to convert a Decimal to the appropriate backend form.
-
-        The difference between DecimalField handling and our handling is that
-        the former uses 'to_python' to cast to Decimal whereas we use
-        'get_prep_value' to cast; essentially DecimalField needs to upcast
-        and we need to downcast.
-        """
-        value = self.get_prep_value(value)
-        return connection.ops.adapt_decimalfield_value(
-            value, self.max_digits, self.decimal_places
-        )
+        if isinstance(value, Bitcoin):
+            return value.satoshis
+        if type(value) == float:
+            raise TypeError("Cannot use float with BitcoinField")
+        return Bitcoin(value).satoshis
